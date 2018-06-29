ea2be1ddfe4b3f7f58510faa78dfc5e0dad4f927
MULTYcode <30332323+MULTYcode@users.noreply.github.com>
2018-06-29 16:41:00 +0700

    update email verify

--- resources/views/email/sendView.blade.php
+++ resources/views/email/sendView.blade.php
@@ -1,1 +1,11 @@
-To verify email <a href="{{route('sendEmailDone',["email"=>$user->email,"verifytoken"=>$user->verifytoken])}}">Click here</a>
+<p>Hi,<br><br>
+
+Thanks for using MyErp Dashboard! Please confirm your email address by clicking on the link below.<br><br>
+
+<a href="{{route('sendEmailDone',["email"=>$user->email,"verifytoken"=>$user->verifytoken])}}">Verified Email</a></p><br><br>
+
+If you did not sign up for a MyErp Dashboard account please disregard this email.</p><br><br>
+
+Happy monitoring! <br>
+The WSM Team
+</p>
