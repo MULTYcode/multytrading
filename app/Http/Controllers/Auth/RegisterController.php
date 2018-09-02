0a04d40ea5a9bf7eef9bf911a3fbf7ec802c7838
MULTYcode <30332323+MULTYcode@users.noreply.github.com>
2018-09-02 10:37:40 +0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/verifyEmailFirst';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
