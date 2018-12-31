bac626aa3852b244ff1c86e4cddfbee1c0065de0
mhdwasiman@gmail.com <mhdwasiman@gmail.com>
2018-12-31 16:32:35 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::namespace('Api')->group(function () {
     Route::post('/login', 'UserController@login');
 
     Route::middleware('auth:api')->group(function(){
-        Route::post('/newtoken', 'Api\UserController@newtoken'); 
+        Route::post('/newtoken', 'UserController@newtoken'); 
         Route::get('/getuser', 'UserController@getuser');
         Route::post('/updateuser', 'UserController@updateuser');
         Route::post('/gantipwduser', 'UserController@gantipwduser');
