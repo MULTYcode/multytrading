141c363483d71af0dfd05fb1aac8b2ef1e86dc4b
mhdwasiman@gmail.com <mhdwasiman@gmail.com>
2018-12-31 17:43:53 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Egulias\EmailValidator\Validation\RFCValidation;
 |
 */
 
-//Route::namespace('Api')->group(function () {
+Route::namespace('Api')->group(function () {
 
     Route::post('/register', 'UserController@register');
     Route::post('/login', 'UserController@login');
@@ -32,6 +32,6 @@ use Egulias\EmailValidator\Validation\RFCValidation;
         Route::get('/getpicuser', 'UserController@getpicuser');
     });
 
-//});
+});
 
 
