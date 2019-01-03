b99491f3eeac3b3e287238f634ff5bb96e7ae610
mhdwasiman@gmail.com <mhdwasiman@gmail.com>
2019-01-03 08:58:21 +0700

    ok

--- app/Http/Controllers/Api/UserCtrl.php
+++ app/Http/Controllers/Api/UserCtrl.php
@@ -19,14 +19,14 @@ class UserCtrl extends Controller
 
             //$activation_code = str_random(60);
     
-            $firstname      = isset($request->input('firstname'));
-            $lastname       = isset($request->input('lastname'));
-            $birth          = isset($request->input('birth'));
-            $address        = isset($request->input('address'));
-            $gender         = isset($request->input('gender'));
-            $phone          = isset($request->input('phone'));
-            $email          = isset($request->input('email'));
-            $password       = isset($request->input('password'));
+            $firstname      = isset($request->firstname);
+            $lastname       = isset($request->lastname);
+            $birth          = isset($request->birth);
+            $address        = isset($request->address);
+            $gender         = isset($request->gender);
+            $phone          = isset($request->phone);
+            $email          = isset($request->email);
+            $password       = isset($request->password);
     
             if(!isset($firstname)){
                 return response()->json(['error'=>true,'msg'=>'First name is empty']);
